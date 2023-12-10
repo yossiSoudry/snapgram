95b3227dbb8bac1e9fcaeec12c4ac2bcb931f286
yossiSoudry <121138149+yossiSoudry@users.noreply.github.com>
2023-12-10 23:05:07 +0200

    0.2

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -18,7 +18,7 @@ const SigninForm = () => {
   const { checkAuthUser, isLoading: isUserLoading } = useUserContext()
   const navigate = useNavigate()
 
-  const { mutateAsync: signInAccount, isPending } = useSignInAccount()
+  const { mutateAsync: signInAccount } = useSignInAccount()
 
   const form = useForm<z.infer<typeof SigninValidation>>({
     resolver: zodResolver(SigninValidation),
